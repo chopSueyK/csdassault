e508170cd5782eb1f1d067cb94990a6a59047947
albaK <aymanbensouira@gmail.com>
2023-04-07 17:40:51 +0200

    fix

--- jetson1.py
+++ jetson1.py
@@ -6,35 +6,25 @@ import cv2
 #### REMARQUES
 # if suddenly unable to access camera, it means another session using this same camera (hung or alive) 
 # reset the nvargus-daemon to reset the argus framework and try nvgstcapture-1.0 again.
-# sudo service nvargus-daemon restart
+# 
+
 # jetson_inference has to be built with cmake -DENABLE_NVMM=off ../
 
 net = jetson.inference.detectNet("ssd-mobilenet-v2", threshold=0.5)
-
-camera= jetson.utils.videoSource("csi://0")
-
-
-""" 
-gstreamer_pipeline returns a GStreamer pipeline for capturing from the CSI camera
-Flip the image by setting the flip_method (most common values: 0 and 2)
-display_width and display_height determine the size of each camera pane in the window on the screen
-Default 1920x1080 displayd in a 1/4 size window
-"""
-
-
-
-display = jetson.utils.videoOutput("video.mp4") 
-
+display = jetson.utils.glDisplay()
 camera = jetson.utils.videoSource("csi://0") 
 
-while display.IsStreaming():
-    
+while True:
+    person = []
     img = camera.Capture()
     detections = net.Detect(img)
     for detection in detections:
-        print(detection) 
-    display.RenderOnce(img)
-    display.SetStatus("Object Detection | Network {:.0f} FPS".format(net.GetNetworkFPS()))
+        if detection.ClassID == 1:
+            print("WE HAVE DETECTED A HUMAN")
+            person.append(detection)
+    print("the amount of people in this frame: {}".format(len(person)))
+    
+    
     print("DONE \n\n\n\n\n\n")
 
 
