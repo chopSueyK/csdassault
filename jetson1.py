0b3f60210f98083853708ba5c4812260c562c37b
chopSueyK <122528348+chopSueyK@users.noreply.github.com>
2023-04-10 23:36:37 +0200

    Update jetson1.py
    import execution fix

--- jetson1.py
+++ jetson1.py
@@ -11,26 +11,26 @@ import time
 # 
 
 # jetson_inference has to be built with cmake -DENABLE_NVMM=off ../
+if __name__ == "__main__":
+    net = jetson.inference.detectNet("ssd-mobilenet-v2", threshold=0.5)
+    display = jetson.utils.glDisplay()
+    #camera = jetson.utils.videoSource("csi://0") 
+    camera = jetson.utils.videoSource("footage.mp4")
 
-net = jetson.inference.detectNet("ssd-mobilenet-v2", threshold=0.5)
-display = jetson.utils.glDisplay()
-#camera = jetson.utils.videoSource("csi://0") 
-camera = jetson.utils.videoSource("footage.mp4")
-
-while True:
-    start = time.time()
-    print("hello")
-    person = []
-    img = camera.Capture()
-    detections = net.Detect(img)
-    end = time.time()
-    duration = (end - start)*1000
-    for detection in detections:
-        if detection.ClassID == 1:
-            print("WE HAVE DETECTED A HUMAN")
-            person.append(detection)
-    print("the amount of people detected in this frame: {} in {}ms".format(len(person), duration))
-    print("FPS = {}".format(1000/duration))
+    while True:
+        start = time.time()
+        print("hello")
+        person = []
+        img = camera.Capture()
+        detections = net.Detect(img)
+        end = time.time()
+        duration = (end - start)*1000
+        for detection in detections:
+            if detection.ClassID == 1:
+                print("WE HAVE DETECTED A HUMAN")
+                person.append(detection)
+        print("the amount of people detected in this frame: {} in {}ms".format(len(person), duration))
+        print("FPS = {}".format(1000/duration))
 
 
-    print("DONE \n\n\n\n\n\n")
+        print("DONE \n\n\n\n\n\n")
